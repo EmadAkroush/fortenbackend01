92d4ce201344ea64fb2fa11412bfced7c36ceb39
Emad <emadakroushwork@gmail.com>
2025-11-01 19:51:22 +0330

    add the origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3500);
   app.enableCors({
-    origin: 'http://localhost:3000', // یا دامنه خاص مثلاً http://72.61.158.39
+    origin: ['https://finalxcard.com', 'https://api.finalxcard.com'], // یا دامنه خاص مثلاً http://72.61.158.39
     credentials: true,
   });
   dotenv.config();
